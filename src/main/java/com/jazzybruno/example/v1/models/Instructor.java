b089c17ffa8f35e6333b85a68ba606b3eaf0e86e
jazzybruno <95282931+jazzybruno@users.noreply.github.com>
2023-04-24 12:00:46 +0200

    Added the annotations and everything to the instructor orm

--- src/main/java/com/jazzybruno/example/v1/models/Instructor.java
+++ src/main/java/com/jazzybruno/example/v1/models/Instructor.java
@@ -1,13 +1,38 @@
 package com.jazzybruno.example.v1.models;
 
+import com.sun.istack.NotNull;
+import lombok.*;
+
+import javax.persistence.*;
 import java.util.Date;
 
+@AllArgsConstructor
+@RequiredArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity
 public class Instructor {
-    private int instructorId;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column
+    private Long instructorId;
+    @NotNull
+    @Column
     private String firstName;
+    @NotNull
+    @Column
     private String lastName;
+    @NotNull
+    @Column
     private Date dateOfBirth;
+    @NotNull
+    @Column
     private String phoneNumber;
+    @NotNull
+    @Column
     private double salary;
+    @NotNull
+    @Column
     private Date remunerationDate;
 }
